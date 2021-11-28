352439daf799f91540c2d73328f9696d6ac34568
ferehibrahimli123@outlook.com <ferehibrahimli123@outlook.com>
2021-11-28 20:11:54 +0400

    visualwork1

---
+++ hmwvisual1/hmwvisual1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace hmwvisual1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //int a = 7;
+            //bool result= false;
+            //for (int n = 1; n < a; n++)
+            //{
+            //    if(a % n == 0)
+            //    {
+            //        result = true;
+            //    }
+            //    else if (a % 2 == 0)
+            //    {
+            //        result = true;
+            //    }
+            //    else
+            //    {
+            //        result = false;
+            //    }
+            //}
+            //if (result)
+            //{
+            //    Console.WriteLine("eded murekkebdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded sadedir");
+            //}
+            #endregion
+
+            #region task2
+            //int num = 333;
+            //int remainder, sum = 0;
+            //while (num > 0)
+            //{
+            //    remainder = num % 10;
+            //    sum = sum + remainder;
+            //    num = num / 10;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+        }
+
+        
+    }
+}
---
+++ task3/task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthorder = 3;
+            Console.WriteLine(findseasonnamebymonthorder(monthorder));
+        }
+        static string findseasonnamebymonthorder(int month)
+        {
+            if (month > 12 || month < 1)
+                return "bele bir ay yoxdur";
+
+            if (month == 3 || month == 4 || month == 5)
+            {
+                return "yaz";
+            }
+
+            else if (month == 6 || month == 7 || month == 8)
+            {
+                return "yay";
+            }
+
+            else if (month == 9 || month == 10 || month == 11)
+            {
+                return "payiz";
+            }
+            else
+            {
+                return "qis";
+            }
+        }
+    }
+}
